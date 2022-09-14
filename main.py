acc44a0995d16762417089d8b6a0f6012c7795be
Niklas Bakula <niklas@MBP-von-Niklas.fritz.box>
2022-09-14 20:58:40 +0200

    Added loop control

--- main.py
+++ main.py
@@ -1,134 +1,207 @@
-#%% User settings___________________________________________________________________________
-
-labels = ['Engine']
-custom_model_name = 'my_ssd_mobilenet_v2_fpnlite'
-
-#%% Import fundamental dependencies_________________________________________________________
+#%% Import fundamental dependencies (which are installed by default or by cloning taxibot repo)
 import os
 import sys
 import time
 from pprint import pprint
 import argparse
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument("-ii",
-#                     "--initalInstallation",
-#                     type=bool, default=False)
-# parser.add_argument("-h",
-#                     "--homePath",
-#                     type=str, default=False)
-# args = parser.parse_args()
+import userSettings
+import setup
 
+#%% System setup
 
+# Load user settings
+userSettings = userSettings.main()
 
-#%% Installation ___________________________________________________________________
-print('{:^75s}'.format('_____Starting project "taxibotAutonomous"_____'))
+# Check whether to run program in first installation mode or default
+parser = argparse.ArgumentParser()
+parser.add_argument("--initialInstallation", type=bool, default=False)
+parser.add_argument("--debugging", type=bool, default=False)
+args = parser.parse_args()
 
-# if args.homePath is None:
-#     home_path = '/Users/niklas/Virtual_Environment/Version_5/projectAutonomous'
-# elif args.homePath is not None:
-#     home_path = args.homePath
-home_path = '/Users/niklas/Virtual_Environment/Version_6/projectTeleoperatedTaxibot'
-print('{:75s}'.format('Home path set to: '+home_path))
+print('{:^75s}'.format('\n_____Starting project "teleoperatedTaxibot"_____\n'))
 
+print('{:75s}'.format('Home path set to: '+userSettings['homePath']))
 
 # Setup folder structure and install extented packages
-import setup
-# if args.initialInstallation == True:
-#     files, paths = setup.installPackages(home_path, labels, firstInstallation=True)
-# elif args.initialInstallation == False:
-#     files, paths = setup.installPackages(home_path, labels, firstInstallation=False)
-
-#%% Loop control
-# print('{:75s}'.format('.'))
-# print('{:75s}'.format('.'))
-# print('{:75s}'.format('Enter "Q" to exit program\nEnter "0" to start object detection\nEnter "1" to calibrate left camera\nEnter "2" to calibrate right camera\nEnter "3" to calibrate stereo setup\n'))
-# print('{:75s}'.format('.'))
-# print('{:75s}'.format('.'))
-# selection = input('Selection: ')
-
-# Quit program
-
-# Start program
-
-# Left camera calibration
-# print('{:75s}'.format('Selected 1: Calibrate left camera'))
-# print('{:75s}'.format('Enter "1" to generate new images\nEnter "2" to find best image combination\nEnter "3" to calibrate camera'))
-
-# # Right camera calibration
-# print('{:75s}'.format('Selected 2: Calibrate right camera'))
-# print('{:75s}'.format('Enter "1" to generate new images\nEnter "2" to find best image combination\nEnter "3" to calibrate camera'))
-
-# # Stereo calibration
-# print('{:75s}'.format('Selected 2: Calibrate right camera'))
-# print('{:75s}'.format('Enter "1" to generate new images\nEnter "2" to find best image combination\nEnter "3" to calibrate camera'))
+if args.initialInstallation == True:
+    files, paths = setup.installPackages(userSettings['homePath'], userSettings['labels'], firstInstallation=True)
+elif args.initialInstallation == False:
+    files, paths = setup.installPackages(userSettings['homePath'], userSettings['labels'], firstInstallation=False)
 
 
-debuggingMode = False
+#%% Camera setup
+print('{:^75s}'.format('\n_____Starting camera setup_____\n'))
 
-stereoGenImages = False
-singleLGenImages = False
-singleRGenImages = False
+# Check whether debugging mode shall be started
+if args.debugging == True:
+    debuggingMode = True
+else:
+    debuggingMode = False
 
-singleLTestCombinations = False
-singleRTestCombinations = False
-stereoTestCombinations = False
-
-singleLCalibration = False
-singleRCalibration = False
-stereoCalibration = False
-
-newRectificationMapping = False
-
-files, paths = setup.installPackages(home_path, labels, firstInstallation=False)
-# Import newly installed dependencies
-print('Importing further packages...')
+# Load settings for camera setup
 import cv2 as cv
 import numpy as np
-
 import cameraSetup as cameraSetup
 
-#%% 0. Check if cameras are displayed correctly_______________________________________
-# capL = cv.VideoCapture(1)
-#capR = cv.VideoCapture(0)
+# Open camera capture
+capL = cv.VideoCapture(1)
+# capR = cv.VideoCapture(0)
 
-if debuggingMode is True:
-    print('Checking camera configuration..')
+# Loop control for camera setup
+cameraSetupCompleted = False
+while cameraSetupCompleted == False:
 
-    while True:
-        key = cv.waitKey(25)
+    print('__Main menu__')
 
-        if key == 27:
-            print('Program was terminated by user..')
-            sys.exit()
-        elif key == ord(' '):
-            print('User confirmed correct arrangement of cameras...')
-            break
+    selectionMain = input('Enter "Q" to exit program\nEnter "0" to check if cameras are connected properly\nEnter "1" to set up left camera\nEnter "2" to set up right camera\nEnter "3" to setup stereo system\nEnter "4" to start program with settings from file\n')
 
-        # Display livestream of camera
-        isTrueL, frameL = capL.read()
-        #isTrueR, frameR = capR.read()
+    # Quit program
+    if selectionMain == 'Q':
+        print('Program terminated by user...')
+        sys.exit()
 
-        cv.imshow('Camera L', frameL)
-        #cv.imshow('Camera R', frameR)
+    # Check if cameras are connected properly
+    elif selectionMain == '0':
 
-    cv.destroyAllWindows()
+        print('Checking camera configuration..')
+        print('Press the ESC key to quit or press SPACE key to confirm correct connection')
+        
+        while True:
+            key = cv.waitKey(25)
 
-#%% 1. Initiate object detection
-print('__________ 1. Loading object dection model __________')
-print('.')
-print('.')
+            if key == 27:
+                print('Going back to main screen. Cameras have been switched')
+                capL = cv.VideoCapture(0)
+                # capR = cv.VideoCapture(1)
+                
+            elif key == ord(' '):
+                print('Correct alignment of cameras confirmed...')
+                break
 
+            # Display livestream of camera
+            isTrueL, frameL = capL.read()
+            #isTrueR, frameR = capR.read()
+
+            cv.imshow('Camera L', frameL)
+            #cv.imshow('Camera R', frameR)
+
+        cv.destroyAllWindows()
+    
+    # Left camera setup
+    elif selectionMain == '1':
+        
+        leftCameraCompleted = False
+        while leftCameraCompleted == False:
+            print('\n____Left camera menu____')
+            selectionLeftCam = input('__Enter "B" to get back to main menue\n__Enter "1" to generate new images\n__Enter "2" to find best image combination\n__Enter "3" to calibrate camera\n')
+
+            # Go back to main menue
+            if selectionLeftCam == 'B':
+                leftCameraCompleted = True
+
+            # Generate new images
+            elif selectionLeftCam == '1':
+                cameraSetup.getIntrinsicsLeftCamera(capL, paths, singleLGenImages=True, singleLTestCombinations=False, singleLCalibration=False, debuggingMode=debuggingMode)
+
+            # Find best combination of images
+            elif selectionLeftCam == '2':
+                cameraSetup.getIntrinsicsLeftCamera(capL, paths, singleLGenImages=False, singleLTestCombinations=True, singleLCalibration=False, debuggingMode=debuggingMode)
+
+            # Find best combination of images
+            elif selectionLeftCam == '3':
+                cameraSetup.getIntrinsicsLeftCamera(capL, paths, singleLGenImages=False, singleLTestCombinations=False, singleLCalibration=True, debuggingMode=debuggingMode)
+                leftCameraCompleted = True
+            else:
+                print('Please make a selection')
+   
+    # Right camera setup
+    elif selectionMain == '2':
+        
+        rightCameraCompleted = False
+        while rightCameraCompleted == False:
+            print('\n____Right camera menu____')
+            selectionRightCam = input('__Enter "B" to get back to main menue\n__Enter "1" to generate new images\n__Enter "2" to find best image combination\n__Enter "3" to calibrate camera\n')
+
+            # Go back to main menue
+            if selectionRightCam == 'B':
+                rightCameraCompleted = True
+
+            # Generate new images
+            elif selectionRightCam == '1':
+                cameraSetup.getIntrinsicsRightCamera(capR, paths, singleRGenImages=True, singleRTestCombinations=False, singleRCalibration=False, debuggingMode=debuggingMode)
+
+            # Find best combination of images
+            elif selectionRightCam == '2':
+                cameraSetup.getIntrinsicsRightCamera(capR, paths, singleRGenImages=False, singleRTestCombinations=True, singleRCalibration=False, debuggingMode=debuggingMode)
+
+            # Find best combination of images
+            elif selectionRightCam == '3':
+                cameraSetup.getIntrinsicsRightCamera(capR, paths, singleRGenImages=False, singleRTestCombinations=False, singleRCalibration=True, debuggingMode=debuggingMode)
+                rightCameraCompleted = True
+            else:
+                print('Please make a selection')
+
+    # Stereo setup
+    elif selectionMain == '3':
+        
+        stereoCompleted = False
+        while stereoCompleted == False:
+            print('\n____Stereo menu____')
+            selectionStereo = input('__Enter "B" to get back to main menue\n__Enter "1" to generate new images\n__Enter "2" to find best image combination\n__Enter "3" to calibrate stereo\n')
+
+            # Go back to main menue
+            if selectionStereo == 'B':
+                stereoCompleted = True
+
+            # Generate new images
+            elif selectionStereo == '1':
+                cameraSetup.calibrateStereoSetup(capL, capR, paths, stereoGenImages=True, stereoTestCombinations=False, stereoCalibration=False, debuggingMode=debuggingMode)
+
+            # Find best combination of images
+            elif selectionStereo == '2':
+                cameraSetup.calibrateStereoSetup(capL, capR, paths, stereoGenImages=False, stereoTestCombinations=True, stereoCalibration=False, debuggingMode=debuggingMode)
+
+            # Calibrate stereo and compute rectification maps
+            elif selectionStereo == '3':
+                Trns = cameraSetup.calibrateStereoSetup(capL, capR, paths, stereoGenImages=False, stereoTestCombinations=False, stereoCalibration=True, debuggingMode=debuggingMode)
+
+                leftMapX, leftMapY, rightMapX, rightMapY, Q, rectifiedCameraMatrixL = cameraSetup.getRectificationMap(capL, capR, paths, newRectificationMapping=True, debuggingMode=debuggingMode)
+                stereoCompleted = True
+
+    # Load parameters from file
+    elif selectionMain == '4':
+
+        # Camera intrinsics
+        cameraSetup.getIntrinsicsLeftCamera(capL, paths, singleLGenImages=False, singleLTestCombinations=False, singleLCalibration=False, debuggingMode=debuggingMode)
+        cameraSetup.getIntrinsicsRightCamera(capR, paths, singleRGenImages=False, singleRTestCombinations=False, singleRCalibration=False, debuggingMode=debuggingMode)
+
+        # Stereo params
+        Trns = cameraSetup.calibrateStereoSetup(capL, capR, paths, stereoGenImages=False, stereoTestCombinations=False, stereoCalibration=False, debuggingMode=debuggingMode)
+        leftMapX, leftMapY, rightMapX, rightMapY, Q, rectifiedCameraMatrixL = cameraSetup.getRectificationMap(capL, capR, paths, newRectificationMapping=False, debuggingMode=debuggingMode)
+        stereoCompleted = True
+
+    else:
+        print('No option was selected. Please try again...')
+
+print('{:^75s}'.format('\n_____Finished camera setup_____\n'))
+
+
+#%% Object detection setup
+print('{:^75s}'.format('\n_____Initiating object detection_____\n'))
+
+print('Importing required modules')
 import tensorflow as tf
 import object_detection
 from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as viz_utils
 from object_detection.builders import model_builder
 from object_detection.utils import config_util
+print('Finished importing modules')
 os.chdir(paths['home'])
 
 # Determine latest checkpoint
-path_model = paths['trainedModels']+'/'+custom_model_name
+path_model = paths['trainedModels']+'/'+userSettings['pretrainedModelName']
 files = os.listdir(path_model)
 last_checkpoint = 0
 for file in files:
@@ -136,7 +209,6 @@ for file in files:
     try:
         ckpt = ckpt.split('-')[1]
     except:
-        # do nothing
         break
     else:
         #print('Current checkpoint: '+ckpt)
@@ -160,44 +232,18 @@ def detect_fn(image):
     detections = detection_model.postprocess(prediction_dict, shapes)
     return detections
 
-# Initiate detection
+
 path_labelmap = path_model+'/label_map.pbtxt'
 category_index = label_map_util.create_category_index_from_labelmap(path_labelmap)
 
-print('.')
-print('.')
-print('__________ 1. Loading object dection model completed __________')
+print('{:^75s}'.format('\n_____Initiation complete_____\n'))
 
 
-## 2. Calibration and rectification
-print('__________ 2. Calibration and rectification __________')
-print('.')
-print('.')
-capL = 0
-capR = 0
-newCameraMatrixL = cameraSetup.getIntrinsicsLeftCamera(capL, paths, singleLGenImages, singleLTestCombinations, singleLCalibration, debuggingMode)
+#%% Main routine
+print('{:^75s}'.format('\n_____Starting taxibot livestream_____\n'))
 
-cameraSetup.getIntrinsicsRightCamera(capR, paths, singleRGenImages, singleRTestCombinations, singleRCalibration, debuggingMode)
-
-Trns = cameraSetup.calibrateStereoSetup(capL, capR, paths, stereoGenImages, stereoTestCombinations, stereoCalibration, debuggingMode)
-
-leftMapX, leftMapY, rightMapX, rightMapY, Q, rectifiedCameraMatrixL = cameraSetup.getRectificationMap(capL, capR, paths, newRectificationMapping, debuggingMode)
-
-
-print('.')
-print('.')
-print('__________ 2. Calibration and rectification completed __________')
-print('.')
-
-## 3. Correspondence (disparity map)
-
-print('.')
-print('__________ 3. Starting correspondence calculation __________')
-
-
-#left_matcher, right_matcher, wls_filter = cameraSetup.initialiseCorrespondence()
-# frameL = cv.imread(paths['stereoTesting']+'/camLTesting.png')
-# frameR = cv.imread(paths['stereoTesting']+'/camRTesting.png')
+# Initiate correspondence algorithms
+left_matcher, right_matcher, wls_filter = cameraSetup.initialiseCorrespondence()
 
 new_frame_time = 0
 prev_frame_time = 0
@@ -217,7 +263,6 @@ while True:
     heightL, widthL, channelsL = Left_rectified.shape
 
     #points_3D, colored_disp = cameraSetup.getDepth(Left_rectified, Right_rectified, Q, debuggingMode, left_matcher, right_matcher, wls_filter)
-
 
     # Detect object on LEFT rectified camera stream
     image_np = np.array(Left_rectified)
@@ -239,7 +284,7 @@ while True:
                 detections['detection_scores'],
                 category_index,
                 use_normalized_coordinates=True,
-                max_boxes_to_draw=1,
+                max_boxes_to_draw=2,
                 min_score_thresh=.8,
                 agnostic_mode=False)
 
