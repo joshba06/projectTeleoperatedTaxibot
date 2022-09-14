2821858da17d0c5e6394e02552b66dcf105c168a
Niklas Bakula <niklas@MBP-von-Niklas.fritz.box>
2022-09-14 17:40:23 +0200

    Added models and required packages to repo

---
+++ userSettings.py
@@ -0,0 +1,12 @@
+
+settings = {
+                'name_of_virtual_environment' : 'Niklas',
+                'path_to_local_interpreter' : '/Users/niklas/Virtual_Environment/Version_6/Niklas/bin/python3',
+
+
+}
+
+
+
+def main():
+    return settings
