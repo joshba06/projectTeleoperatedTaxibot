ef9330471ca30cba3ee556daf4b47f949ba46403
Niklas Bakula <niklas@MacBook-Pro-von-Niklas.fritz.box>
2022-09-18 00:36:47 +0200

    Now supports skipping of invisible files

--- 0_UserInput/scripts/imageProcessing.py
+++ 0_UserInput/scripts/imageProcessing.py
@@ -1,10 +1,6 @@
-#%% README
-# This document contains the main functions for the image processing step. It is ONLY run on colab, hence image is plotted using plt.imshow and not cv2.imshow!
-##__________________
-
 #%% Imports
 import os
-import cv2
+import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 import uuid
@@ -20,6 +16,11 @@ import random
 import shutil
 
 import albumentations as A
+
+def listdirVisible(path):
+    items = os.listdir(path)
+    visible = [item for item in items if not item.startswith('.')]
+    return visible
 
 def clearPreprocessing(path_preprocessing):
     
@@ -142,18 +143,18 @@ def visualize_bbox(img, bbox, class_name, color=BOX_COLOR, thickness=2):
     x_min, y_min, w, h = bbox
     x_min, x_max, y_min, y_max = int(x_min), int(x_min + w), int(y_min), int(y_min + h)
    
-    cv2.rectangle(img, (x_min, y_min), (x_max, y_max), color=color, thickness=thickness)
+    cv.rectangle(img, (x_min, y_min), (x_max, y_max), color=color, thickness=thickness)
     
-    ((text_width, text_height), _) = cv2.getTextSize(class_name, cv2.FONT_HERSHEY_SIMPLEX, 0.35, 1)    
-    cv2.rectangle(img, (x_min, y_min - int(1.3 * text_height)), (x_min + text_width, y_min), BOX_COLOR, -1)
-    cv2.putText(
+    ((text_width, text_height), _) = cv.getTextSize(class_name, cv.FONT_HERSHEY_SIMPLEX, 0.35, 1)    
+    cv.rectangle(img, (x_min, y_min - int(1.3 * text_height)), (x_min + text_width, y_min), BOX_COLOR, -1)
+    cv.putText(
         img,
         text=class_name,
         org=(x_min, y_min - int(0.3 * text_height)),
-        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+        fontFace=cv.FONT_HERSHEY_SIMPLEX,
         fontScale=0.35, 
         color=TEXT_COLOR, 
-        lineType=cv2.LINE_AA,
+        lineType=cv.LINE_AA,
     )
     return img
 
@@ -163,14 +164,14 @@ def visualize(image, bboxes, category_ids, category_id_to_name):
     for bbox, category_id in zip(bboxes, category_ids):
         class_name = category_id_to_name[category_id]
         img = visualize_bbox(img, bbox, class_name)
-    plt.figure(figsize=(8, 8))
+    plt.figure(figsize=(5, 5))
     plt.axis('off')
     plt.imshow(img)
 
 def place_object(background, object, x_koord, y_koord, width, height):
     
     # Resize object and neutralise alpha channel
-    obj_resized = cv2.resize(object, (width, height))
+    obj_resized = cv.resize(object, (width, height))
     alpha_s = obj_resized[:, :, 3] / 255
     alpha_l = 1.0-alpha_s
     
@@ -184,31 +185,27 @@ def place_object(background, object, x_koord, y_koord, width, height):
 transform = A.Compose([
     A.RandomBrightnessContrast(brightness_limit=0.3, contrast_limit=0.3, brightness_by_max=True, always_apply=False, p=0.33),
     A.RGBShift(always_apply=False, p=0.66, r_shift_limit=30, g_shift_limit=30, b_shift_limit=30),
-    A.Rotate(limit=45, p=0.66, border_mode=cv2.BORDER_CONSTANT),
+    A.Rotate(limit=45, p=0.66, border_mode=cv.BORDER_CONSTANT),
 ], bbox_params=A.BboxParams(format='coco', label_fields=['category_ids']))
 
 
 
 def main(dict_labels, path_backgrounds, path_objects, numImgs, upperScale, lowerScale, save_folder, path_training):
+    '''
+    @params
+    @labels: dictionary with label ids and names
+    @path_backgrounds: Path to 'backgrounds' folder
+    @path_objects: Path to folder where all images for one label are saved
+    '''
 
-    # labels: dictionary with label ids and names
-    # path_backgrounds: Path to 'backgrounds' folder
-    # path_object: Path to folder where all images for one label are saved
 
-    # Get backgrounds
-    backgrounds = os.listdir(path_backgrounds)
+    # Get background image paths
+    backgrounds = listdirVisible(path_backgrounds)
     n_backgrounds = len(backgrounds)
     print('Found '+str(n_backgrounds)+' backgrounds')
 
-    # Get all object folders
-    object_folders = os.listdir(path_objects)
-
-    # Remove .DS_Store files from object_folders (on local machine)
-    try:
-        object_folders.remove('.DS_Store')
-    except:
-        #print('No file called .DS_Store found in array')
-        flag = 0
+    # Get all object folder paths
+    object_folders = listdirVisible(path_objects)
   
     lslabels = list(dict_labels.keys())
 
@@ -221,34 +218,26 @@ def main(dict_labels, path_backgrounds, path_objects, numImgs, upperScale, lower
             
             # Get all images for current label
             path_images = path_objects+'/'+label
-            list_images = os.listdir(path_images)
-
-            # Remove .DS_Store files from object_folders (on local machine)
-            try:
-                list_images.remove('.DS_Store')
-            except:
-                #print('No file called .DS_Store found in array')
-                flag = 0
-
+            list_images = listdirVisible(path_images)
             n_images = len(list_images)
             print('Found '+str(n_images)+' objects for label: '+label+'. Multiplying by factor '+str(numImgs))
 
             # Loop through all images
             for i in range(0, n_images):
                 
-                ## Get object
+                # Get object
                 path_object = path_images+'/'+list_images[i]
                 
                 # Open object as CMYK and get alpha-channel
-                obj_cmyk = cv2.imread(path_object, cv2.IMREAD_UNCHANGED)
+                obj_cmyk = cv.imread(path_object, cv.IMREAD_UNCHANGED)
                 alpha = obj_cmyk[:,:,3]
 
                 # Open object second time as BGR and convert to RGB
-                obj_bgr = cv2.imread(path_object, cv2.IMREAD_COLOR)
-                b, g, r = cv2.split(obj_bgr)
+                obj_bgr = cv.imread(path_object, cv.IMREAD_COLOR)
+                b, g, r = cv.split(obj_bgr)
 
                 # Add alpha-channel to RGB 
-                obj_rgb = cv2.merge([r, g, b, alpha])
+                obj_rgb = cv.merge([r, g, b, alpha])
 
                 # Get object information
                 obj_name = list_images[i].split('_')[0]
@@ -263,8 +252,8 @@ def main(dict_labels, path_backgrounds, path_objects, numImgs, upperScale, lower
                     x = np.random.randint(n_backgrounds)
                     bg_name = backgrounds[x].split('.')[0]
                     path_background = path_backgrounds+'/'+str(bg_name)+'.png'
-                    background = cv2.imread(path_background, 1)
-                    background = cv2.cvtColor(background, cv2.COLOR_BGR2RGB)
+                    background = cv.imread(path_background, 1)
+                    background = cv.cvtColor(background, cv.COLOR_BGR2RGB)
                     bnd_width, bnd_height, _ = background.shape
 
                     # Generate random size of object and determine random position on background
@@ -273,31 +262,19 @@ def main(dict_labels, path_backgrounds, path_objects, numImgs, upperScale, lower
                     x_rand = np.random.randint(bnd_width-(rand_size / obj_ratio+1))
                     
                     # Place object on chosen background with random size and coordinates
-
-                    #  Xmin and Ymin
                     image_result = place_object(background, obj_rgb, x_rand, y_rand, int(rand_size/obj_ratio), rand_size)
             
                     # Save chosen coordinates for bboxes
                     bboxes = [[x_rand, y_rand, int(rand_size/obj_ratio), rand_size]]
-                    
-                    ##  __________________________
-                    # Format required: category_id_to_name = {1: 'Mug'}
-                    
-                    category_ids = []
-                    # Current id
-                    category_ids.append(dict_labels[label])
-                    #print(category_ids)
 
+                    # Set up category ids
+                    category_ids = []
+                    category_ids.append(dict_labels[label])
                     category_id_to_name = {}
                     category_id_to_name[category_ids[0]]=label
-                    #print(category_id_to_name)
-                    
-                    ##____________________
 
-                    # Visualise temporary result
-                    # visualize(image_result, bboxes, category_ids, category_id_to_name)
 
-                    # Modify image randomly with Albumentation
+                    # Modify image with Albumentation
                     random.seed()
                     transformed = transform(image=image_result, bboxes=bboxes, category_ids=category_ids)
                     # visualize(
@@ -310,14 +287,13 @@ def main(dict_labels, path_backgrounds, path_objects, numImgs, upperScale, lower
                     image_transformed = transformed['image']
                     bbox_transformed = transformed['bboxes'][0]
 
-                    # Temporarily save images to 'Preprocessing' folder
+                    # Save images to 'Preprocessing' folder
                     file_name = obj_name + str(uuid.uuid1())
                     img_width, img_height, img_depth = image_transformed.shape
                     save_path = save_folder+label
-                    #print('Save path: '+save_path)
+
                     abs_path = os.path.abspath(save_path)
 
-                    
                     x_min = bbox_transformed[0]
                     y_min = bbox_transformed[1]
                     bbx_width = bbox_transformed[2]
@@ -329,16 +305,15 @@ def main(dict_labels, path_backgrounds, path_objects, numImgs, upperScale, lower
                     xml_File.write(annotation)
                     xml_File.close()
                     #print('Saving image "'+file_name+'" and xml file to directory: '+str(save_path))
-                    cv2.imwrite(save_path +'/'+ file_name + '.jpg', cv2.cvtColor(image_transformed, cv2.COLOR_RGB2BGR))
+                    cv.imwrite(save_path +'/'+ file_name + '.jpg', cv.cvtColor(image_transformed, cv.COLOR_RGB2BGR))
 
                     # Add images and their respective xml files to array
                     all_items.append([save_path +'/'+ file_name + '.jpg', save_path +'/'+ file_name + '.xml'])
 
 
-        ## Partition images to testing / training folders
+        ## Partition images to testing / training folders from 1_Preprocessing
         
         # Count number of images in each label folder
-        #print(all_items)
         n_items = len(all_items)
 
         # Use 15% of the images for testing, 85% for training
@@ -347,7 +322,7 @@ def main(dict_labels, path_backgrounds, path_objects, numImgs, upperScale, lower
         n_training = n_items - n_testing
         print('Multiplication completed. Currently available for label: '+str(label)+', total: '+str(n_items)+', testing: '+str(n_testing)+', training: '+str(n_training))      
               
-        # Parition images for training and testing in a random order
+        # Partition images for training and testing in a random order
         
         # Training
         count = 0
@@ -357,7 +332,6 @@ def main(dict_labels, path_backgrounds, path_objects, numImgs, upperScale, lower
             number_history.append(randnum)
             source_jpg = all_items[randnum][0]
             source_xml = all_items[randnum][1]
-            #print('Copying image" '+str(source_jpg)+'" to training folder')
           
             # Ignore hidden files, such as .ds_store
             if not (all_items[randnum][0].startswith('.') or all_items[randnum][1].startswith('.')):
@@ -366,10 +340,7 @@ def main(dict_labels, path_backgrounds, path_objects, numImgs, upperScale, lower
                 shutil.copy(source_jpg, training)
                 shutil.copy(source_xml, training)
                 all_items.pop(randnum)
-                count +=1
-        # print('Random numbers:')
-        # print(number_history)
-        #print('Copied '+str(count)+' images to training folder')       
+                count +=1 
 
         # Testing
         count = 0
@@ -379,7 +350,6 @@ def main(dict_labels, path_backgrounds, path_objects, numImgs, upperScale, lower
             number_history.append(randnum)
             source_jpg = all_items[randnum][0]
             source_xml = all_items[randnum][1]
-            #print('Copying image" '+str(source_jpg)+'" to testing folder')
             
             # Ignore hidden files, such as .ds_store
             if not (all_items[randnum][0].startswith('.') or all_items[randnum][1].startswith('.')) :
@@ -388,6 +358,3 @@ def main(dict_labels, path_backgrounds, path_objects, numImgs, upperScale, lower
                 shutil.copy(source_xml, testing)
                 all_items.pop(randnum)
                 count +=1
-        # print('Random numbers:')
-        # print(number_history)
-        #print('Copied '+str(count)+' images to testing folder')  
